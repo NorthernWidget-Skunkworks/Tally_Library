a4871a47e7226dd55ee859c28901a6c34ae3ed88
Bobby Schulz <schu3119@umn.edu>
2019-06-27 11:43:36 -0500

    Fixed formatting and web link in example

--- examples/Tally_I2C_Read/Tally_I2C_Read.ino
+++ examples/Tally_I2C_Read/Tally_I2C_Read.ino
@@ -4,7 +4,7 @@ Demo sketch for demonstrating the use of the Tall_I2C library
 For use with Tally > v1.0
 Bobby Schulz @ Northern Widget LLC
 6/26/2019
-https://github.com/NorthernWidget-Skunkworks/Tally_Library/examples
+https://github.com/NorthernWidget-Skunkworks/Tally_Library
 
 "On two occasions I have been asked, 'Pray, Mr. Babbage, if you put into the machine wrong figures, will the right answers come out?' 
 I am not able rightly to apprehend the kind of confusion of ideas that could provoke such a question."
@@ -23,11 +23,11 @@ void setup() {
 	uint8_t Stat = false; //Used to test for connectivity to device 
 	Serial.begin(9600); //Initialize serial communication 
 	Serial.println("Welcome to the Counting Machine...");  //Obligatory welcome
-  	Serial.print("Period = "); Serial.print(Period); Serial.println(" ms"); //Display preset period
-  	Serial.print("Status = ");  //Prints status of device, if device is detected, prints PASS, otherwise prints ERROR
-  	Stat = Counter.begin();
-  	if(Stat == 0) Serial.println("PASS");
-  	else Serial.println("ERROR");
+	Serial.print("Period = "); Serial.print(Period); Serial.println(" ms"); //Display preset period
+	Serial.print("Status = ");  //Prints status of device, if device is detected, prints PASS, otherwise prints ERROR
+	Stat = Counter.begin();
+	if(Stat == 0) Serial.println("PASS");
+	else Serial.println("ERROR");
 	Counter.Clear(); //Clear device count on startup to ensure first reading is valid
 }
 
